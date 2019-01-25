1976b1b5c94c757498fb27054543a58a8b9360f5
Christoph Gruener <christophgruener@me.com>
2019-01-25 22:26:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,5 +12,5 @@ end
   
 def printer(attendees)
   print_badge_times = batch_badge_creator(attendees).length-1
-  print_badge_times.map {|x| puts {batch_badge_creator(x)}
+  print_badge_times.map {|x| puts {batch_badge_creator(attendees)}
 end
