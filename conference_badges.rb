7d320df0a64e29a805bfb997bc10645bce41a0a1
Christoph Gruener <christophgruener@me.com>
2019-01-25 21:39:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map{|x| badge_maker(x)}
+end
+
+def assign_rooms(speakers)
+  speakers.map.with_index { |x, index| "Hello, #{x}! You'll be assigned to room #{index+1}!"}
+end
+  
+def printer(speakers)
+  batch_badge_creator
+end
